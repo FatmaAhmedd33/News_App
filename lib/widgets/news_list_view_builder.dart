ea9e6ba68b3ff26a15d960f66407b1e42cb0e601
fatma <faatmaaahmedd12@gmail.com>
2024-02-28 16:39:49 +0200

    refactor the code

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,54 +1,60 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/classes/articles_madel.dart';
 import 'package:news_app/service/news_service.dart';
-
+import '../classes/articles_madel.dart';
 import 'news_list_view.dart';
 
-
-class NewsListViewBuilder extends StatelessWidget {
+class NewsListViewBuilder extends StatefulWidget {
   const NewsListViewBuilder({super.key});
 
-  /*List<ArticlesModel> articles = [];
-  bool isLoading = true; //the loading doesnt begin
+  @override
+  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
+}
+
+class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
+  var future;
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
-    getNewsGeneral();
+
+    future = NewsService(Dio()).getGeneral();
   }
 
-  Future<void> getNewsGeneral() async {
-    articles = await NewsService(Dio()).getGeneral();
-    isLoading = false; //the loading end
-    setState(() {});
-  }*/
-
-  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: NewsService(Dio()).getGeneral(),
-        builder: (context, snapshot) 
-        {
-         return NewsListView(
-            articles: snapshot.data ?? [],
-          );
-        },);
-
-    /*isLoading
-        ? SliverToBoxAdapter(
-            child: Container(
+    return FutureBuilder<List<ArticlesModel>>(
+        future: future,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return NewsListView(
+              articles: snapshot.data!,
+            );
+          } else if (snapshot.hasError) {
+            return const SliverToBoxAdapter(
+              child: ErrorMassage(),
+            );
+          } else {
+            return SliverToBoxAdapter(
+              child: SizedBox(
                 // to make the load indicator in middle of screen
 
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height / 2,
-                child: const Center(child: CircularProgressIndicator())),
-          )
-        : articles.isNotEmpty
-            ? NewsListView(
-                articles: articles,
-              )
-            : const SliverToBoxAdapter(
-                child: Text("oops theres an error here"),
-              );*/
+                child: const Center(child: CircularProgressIndicator()),
+              ),
+            );
+          }
+        });
+  }
+}
+
+class ErrorMassage extends StatelessWidget {
+  const ErrorMassage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("oops theres an error here");
   }
 }
